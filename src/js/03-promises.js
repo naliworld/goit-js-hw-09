6f3501fa76eb7c86c28f914a5e7a299ee52e5fa1
naliworld <123972576+naliworld@users.noreply.github.com>
2023-07-19 15:48:14 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
+ 
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
+
+const options = {
+  width: '300px',
+  fontSize: '16px',
+  position: 'center',
+  borderRadius: '10px',
+  clickToClose: true,
+};
+
+function onSubmitForm(e){
+  e.preventDefault();
+
+  let delay = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+    .then(({position, delay}) => {
+      setTimeout(() => {
+        Notify.success(
+          `Fulfilled promise ${position}' in ${delay}ms`,
+          options
+        );
+      }, delay);
+    })
+    .catch(({position, deklay}) => {
+      setTimeout(() => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`, options);
+      }, delay);
+    });
+    delay += step;
+  }
+  e.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const objectPromise = {promise, delay};
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(objectPromise);
+    }
+    reject(objectPromise);
+  });
 }
