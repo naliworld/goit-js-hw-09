56608b6c19776cc3efec3425c8ea1c6619ee73ba
naliworld <123972576+naliworld@users.noreply.github.com>
2023-07-21 12:18:49 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import 'flatpickr/dist/themes/dark.css';
-//import { Report } from 'notiflix/build/notiflix-report-aio';//
+
 
 const calendar = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
